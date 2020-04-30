5d6acf67a71d6a07a8699b861f1f073ad6ef295e
Rombik-dev-base <61863339+Rombik-dev-base@users.noreply.github.com>
2020-04-30 10:54:50 +0300

    Create main.cpp

---
+++ 13Lab/A/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+ 
+using namespace std;
+ 
+int main() {
+    ifstream inp("search1.in");
+    ofstream outp("search1.out");
+    string input;
+    string input2;
+    inp >> input;
+    inp >> input2;
+    input2 = input +"l"+input2;
+    vector<int> ans;
+    vector<int> prefix(input2.size(), 0);
+    for(int i = 1 ; i < input2.size(); i++){
+        int curprefix = prefix[i-1];
+        while(curprefix && input2[i] != input2[curprefix])
+            curprefix = prefix[curprefix - 1];
+        if(input2[i] == input2[curprefix])
+            curprefix += 1;
+        if(curprefix == input.size())
+            ans.push_back(i - 2*input.size() + 1);
+        prefix[i] = curprefix;
+    }
+    outp << ans.size() << '\n';
+    for(auto i : ans){
+        outp << i << ' ';
+    }
+}
